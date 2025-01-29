a81f9739a41a0d23cccc83001244811bdccd4429
Yeimss <jamesherrera364@gmail.com>
2025-01-28 23:39:44 -0500

    Reto cambio de titulo

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,15 @@
 import './TodoCounter.css';
 
 function TodoCounter({total, completed}){
+  if(total === completed){
     return (
+      <h1 className='checkAll'>
+        ¡Sos el más grande de todos! <span>Lo completaste todooo</span>
+      </h1>
+    );  
+  }  
+  
+  return (
       <h1>
         Completaste <b>{completed}</b> de <b>{total}</b> TODOs
       </h1>
